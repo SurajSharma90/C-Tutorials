593072dd3b5b3933b4a3b688565dafe203ecc432
Headturna <suraj-sharma@live.com>
2018-06-03 16:03:15 +0200

    first

---
+++ c++ tutorial/c++ tutorial/main.cpp
@@ -0,0 +1,15 @@
+//INCLUDE A LIBRARY
+#include<iostream>
+
+//MAIN FUNCTION
+int main() //FUNCTION HEAD (RETURN-TYPE, NAME, PARAMETERS)
+{
+	//LEFT TO RIGHT AND DOWN
+	
+	//PRINT TO THE CONSOLE
+	std::cout << "HELLO WORLD!" << "\n";
+
+	system("PAUSE");
+
+	return 0; //ENDS THE PROGRAM
+}
