f6b0bd53d5a50325ac2b45e4aa65c326aff3cdbf
Headturna <suraj-sharma@live.com>
2018-06-06 22:06:33 +0200

    Tutorial 2

---
+++ c++ tutorial/c++ tutorial/Tutorial1.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//
+//int main()
+//{
+//	int age = 0;
+//	int moneys = 0;
+//
+//	std::cout << "Please type an age and how much cash monies: ";
+//	std::cin >> age >> moneys;
+//
+//	std::cout << "The age is: " << age << "\n" << "Money is: " << moneys << "\n";
+//
+//	system("PAUSE");
+//
+//	return 0;
+//}
---
+++ c++ tutorial/c++ tutorial/Tutorial2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	//LEFT TO RIGHT AND THEN DOWN
+	//TYPE - NAME - VALUE
+	int i = 34; //Initializing a variable (SCOPE!) (Turncates, cuts off the decimal)
+	int k = 20;
+	int result = i + k;
+	double d = 30.54566; //Double precision variable (Rounds up the number for you)
+	float f = 20.593453453; //Floating point variable (Rounds up the number for you)
+	char c = 'a'; //Character (ASCII TABLE)
+	bool b = false; //Boolean varialbe (TRUE OR FALSE)
+	string s = "Hello I am a string variable!"; //String variable
+
+	cout << result << "\n";
+
+	system("PAUSE");
+
+	return 0;
+}
