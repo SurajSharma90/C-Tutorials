460be097a1f34b9dd1aed0c699b2d1bb80cc0ebf
Headturna <suraj-sharma@live.com>
2018-06-14 17:40:42 +0200

    new

---
+++ c++ tutorial/c++ tutorial/Tutorial4.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+	int i = 0;
+	std::string sentence = "";
+	bool value = false;
+
+	std::cout << "Input a number friend: ";
+	std::cin >> i;
+	std::cin.ignore();
+
+	std::cout << "Input a sentence: ";
+	std::getline(std::cin, sentence);
+
+	//RELATIONAL OPERATORS 
+	/*
+	== (Equals) 
+	>= (Greater or Equals) 
+	<= (Less than or Equals) 
+	< (Less than) 
+	> (Greater than) 
+	! (Is not True)
+	!= (Is not Equal)
+	*/ 
+
+	if (value)
+	{
+		std::cout << "Value is FALSE!" << "\n";
+	}
+	else
+	{
+		std::cout << "Value is TRUE!" << "\n";
+	}
+
+	if (i == 23)
+	{
+		std::cout << "The integer is 23" << "\n";
+	}
+	else if (i < 23)
+	{
+		std::cout << "The integer is less than 23!" << "\n";
+	}
+	else if (i == 25)
+	{
+		std::cout << "WOW the integer is huge!" << "\n";
+	}
+	else
+	{
+		std::cout << "The integer was none of the above values!" << "\n";
+	}
+
+	std::cout << "The integers value is: " << i << "\n";
+
+	if (sentence == "hello")
+	{
+		std::cout << "The sentence is: " << sentence << "\n";
+	}
+
+	system("PAUSE");
+	return 0;
+}
