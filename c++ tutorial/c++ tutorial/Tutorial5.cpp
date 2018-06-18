226157869590684a58ae8898b58a8511f6403a7c
Headturna <suraj-sharma@live.com>
2018-06-18 17:14:13 +0200

    new stuff

---
+++ c++ tutorial/c++ tutorial/Tutorial5.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//
+//int main()
+//{
+//	int colorChoice = 0;
+//
+//	std::cout << "Pick a color (1: R, 2: G, 3: B): ";
+//	std::cin >> colorChoice;
+//
+//	switch (colorChoice)
+//	{
+//	case 1:
+//	
+//		std::cout << "Color RED picked!" << "\n";
+//		
+//		break;
+//	
+//	case 2:
+//
+//		std::cout << "Color GREEN picked!" << "\n";
+//
+//		break;
+//
+//	case 3:
+//
+//		std::cout << "Color BLUE picked!" << "\n";
+//
+//		break;
+//
+//	default:
+//		std::cout << "Not a valid choice!" << "\n";
+//		break;
+//	}
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ c++ tutorial/c++ tutorial/Tutorial6.cpp
@@ -0,0 +1,24 @@
+//#include<iostream>
+//
+//int main()
+//{
+//	
+//	int choice = 1;
+//
+//	//while loop
+//	//while (choice <= 0)
+//	//{
+//	//	std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
+//	//	std::cin >> choice;
+//	//}
+//
+//	//do-while loop
+//	do
+//	{
+//		std::cout << "Quit program? (0: No / 1: Yes)" << "\n";
+//		std::cin >> choice;
+//	} 
+//	while (choice <= 0);
+//
+//	return 0;
+//}
