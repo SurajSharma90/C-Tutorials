f1bd6728fad0701c5a910595ce00f20ba78be89e
Headturna <suraj-sharma@live.com>
2018-06-23 22:41:35 +0200

    new

--- c++ tutorial/c++ tutorial/Tutorial7.cpp
+++ c++ tutorial/c++ tutorial/Tutorial7.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
-
-int main()
-{
-	//keyword (iterator name and start value ; condition ; iterator increment)
-	for (int i = 0; i < 7; i++)
-	{
-		for (int k = 0; k < 7; k++)
-		{
-			std::cout << i << " " << k << "\n";
-		}
-	}
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//	//keyword (iterator name and start value ; condition ; iterator increment)
+//	for (int i = 0; i < 7; i++)
+//	{
+//		for (int k = 0; k < 7; k++)
+//		{
+//			std::cout << i << " " << k << "\n";
+//		}
+//	}
+//
+//	system("PAUSE");
+//	return 0;
+//}
---
+++ c++ tutorial/c++ tutorial/Tutorial8.cpp
@@ -0,0 +1,59 @@
+//#include<iostream>
+//
+//int main()
+//{
+//	//Arithmetic operators
+//
+//	int x = 0;
+//	int y = 23;
+//
+//	float f1 = 6.f;
+//	float f2 = 4.f;
+//	
+//	x = y + 2;
+//	std::cout << "y+2 " << x << "\n";
+//	x = 2 + 3;
+//	std::cout << "+ " << x << "\n";
+//	x = 5 - 4;
+//	std::cout << "- " << x << "\n";
+//	x = 4 * 3;
+//	std::cout << "* " << x << "\n";
+//	x = 6 / 4;
+//	std::cout << "/ " << x << "\n";
+//	x = 6 % 4;
+//	std::cout << "% " << x << "\n";
+//
+//	std::cout << "Proper division " << f1/f2 << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}#include<iostream>
+//
+//int main()
+//{
+//	//Arithmetic operators
+//
+//	int x = 0;
+//	int y = 23;
+//
+//	float f1 = 6.f;
+//	float f2 = 4.f;
+//	
+//	x = y + 2;
+//	std::cout << "y+2 " << x << "\n";
+//	x = 2 + 3;
+//	std::cout << "+ " << x << "\n";
+//	x = 5 - 4;
+//	std::cout << "- " << x << "\n";
+//	x = 4 * 3;
+//	std::cout << "* " << x << "\n";
+//	x = 6 / 4;
+//	std::cout << "/ " << x << "\n";
+//	x = 6 % 4;
+//	std::cout << "% " << x << "\n";
+//
+//	std::cout << "Proper division " << f1/f2 << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
