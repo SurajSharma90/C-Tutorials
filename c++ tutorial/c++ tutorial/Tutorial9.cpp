d984dac5e87e3b5573098130dd9b70260637e318
Headturna <suraj-sharma@live.com>
2018-06-25 20:04:08 +0200

    new

--- c++ tutorial/c++ tutorial/Tutorial9.cpp
+++ c++ tutorial/c++ tutorial/Tutorial9.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-
-int main()
-{
-	//Compound assignment & Increment/Decrement
-	int i = 20;
-	int k = 10;
-	std::cout << i << "\n";
-	i += k; //Short way of writing i = i + k
-	i *= k;
-	i /= 2;
-	std::cout << i << "\n";
-
-	//++i = prefix (Use value before change)
-	//i++ = postfix (Change value before use)
-	
-	k = ++i;
-
-	std::cout << k << "\n";
-	std::cout << i << "\n";
-
-	system("PAUSE");
-	return 0;
-}
+//#include<iostream>
+//
+//int main()
+//{
+//	//Compound assignment & Increment/Decrement
+//	int i = 20;
+//	int k = 10;
+//	std::cout << i << "\n";
+//	i += k; //Short way of writing i = i + k
+//	i *= k;
+//	i /= 2;
+//	std::cout << i << "\n";
+//
+//	//++i = prefix (Use value before change)
+//	//i++ = postfix (Change value before use)
+//	
+//	k = ++i;
+//
+//	std::cout << k << "\n";
+//	std::cout << i << "\n";
+//
+//	system("PAUSE");
+//	return 0;
+//}
